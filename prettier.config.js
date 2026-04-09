d3a7e05eb9f39b3d4a4fd767dc00857cbefdc3be
Filipe520 <filipealves520@gmail.com>
2026-04-09 19:37:53 -0300

    começando projeto

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 60, // diminui a largura máxima para forçar mais quebras
+  tabWidth: 2,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "ignore",
+};
